abb27d38860a7efac99bb98032b2b9746adfb6dd
Fernando <fernandorato.0@hotmail.com>
2020-02-14 08:59:35 -0300

    Adicionado estrutura basica mobile

---
+++ Mobile/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function src() {
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    );
+}
