4fb7217b307468a63fa3b848c57b84562a4ecd67
Fernando <fernandorato.0@hotmail.com>
2020-02-14 14:58:05 -0300

    Adicionado Stilo na tela de SignUp

--- Mobile/src/pages/SignUp/index.js
+++ Mobile/src/pages/SignUp/index.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
 import Background from '~/components/Background';
 
-export default function SignUp() {
+import logo from '~/assets/logo.png';
+
+import {
+    Container,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLink,
+    SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
     return (
         <Background>
-            <Text>SignUp</Text>
+            <Container>
+                <Image source={logo} />
+
+                <Form>
+                    <FormInput
+                        icon="person-outline"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Nome Completo"
+                    />
+
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Digite seu e-mail"
+                    />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Digite sua senha"
+                    />
+
+                    <SubmitButton onPress={() => { }}>Acessar</SubmitButton>
+                </Form>
+
+                <SignLink onPress={() => navigation.navigate('SignIn')}>
+                    <SignLinkText>Já Tenho conta!</SignLinkText>
+                </SignLink>
+            </Container>
         </Background>
     );
 }
