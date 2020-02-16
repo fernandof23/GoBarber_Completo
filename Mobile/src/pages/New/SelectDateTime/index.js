626cda72f86048cec605b75e70b83dd2b62ba05d
Fernando <fernandorato.0@hotmail.com>
2020-02-16 17:19:28 -0300

    criando dateinput

--- Mobile/src/pages/New/SelectDateTime/index.js
+++ Mobile/src/pages/New/SelectDateTime/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import DateInput from '~/components/DateInput';
+
+import { Container } from './styles';
+
 import Background from '~/components/Background';
 
 export default function SelectDateTime() {
     return (
         <Background>
-            <Text>New Date</Text>
+            <Container>
+                <DateInput />
+            </Container>
         </Background>
     );
 }
+
+SelectDateTime.navigationOptions = ({ navigation }) => ({
+    title: 'Selecione o horario',
+    headerLeft: () => (
+        <TouchableOpacity
+            onPress={() => {
+                navigation.goBack();
+            }}
+        >
+            <Icon name="chevron-left" size={20} color="#fff" />
+        </TouchableOpacity>
+    ),
+});
